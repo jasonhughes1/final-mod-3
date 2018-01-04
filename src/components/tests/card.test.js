48e0903f1ee6c94ffb23fc805e1064a3729fda3d
Jason Hughes <jasonhughes088@gmail.com>
2018-01-04 15:49:25 -0700

    tests added but card test is not passing

--- src/components/tests/card.test.js
+++ src/components/tests/card.test.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import Card from '../Card/Card';
-import { shallow } from 'enzyme';
+import { shallow, mount, jest } from 'enzyme';
 import houseMock from './mockData/houseMock';
 
 describe('Card Tests', () => {
 
-
-  it('should be defined', () => {
-    const renderedCard = shallow(
-      <Card house={houseMock[0]} />);
-
-    expect(renderedCard).toBeDefined();  
-  });
-
   it('should render component and match snapshot', () => {
+    let mockFunc = jest.fn();
     const renderedCard = shallow(
-      <Card house={houseMock[0]} />);
+      <Card house={houseMock[0]}
+        mockFunc={mockFunc}
+      />);
 
     expect(renderedCard).toMatchSnapshot();
+  });
+
+  it('should render component', () => {
+    const mockFunc = jest.fn();
+    const renderedCard = mount(
+      <Card house={houseMock[0]}
+        mockFunc={mockFunc}
+      />);
+
+    const div = renderedCard.find('.card');
+
+    expect(renderedCard.state().displayMembers).toEqual(false);
+    div.simulate('click');
+    expect(renderedCard.state().displayMembers).toEqual(true);
   });
 });
